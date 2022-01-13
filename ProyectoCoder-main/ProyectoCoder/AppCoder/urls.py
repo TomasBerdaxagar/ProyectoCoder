1afc333e079c6508f64c8be59b4d7398a60c069f
TomasBerdaxagar <94941276+TomasBerdaxagar@users.noreply.github.com>
2022-01-12 22:02:21 -0300

    Update urls.py

--- ProyectoCoder-main/ProyectoCoder/AppCoder/urls.py
+++ ProyectoCoder-main/ProyectoCoder/AppCoder/urls.py
@@ -1,20 +1,33 @@
 from django.urls import path #Importamos el path
 from AppCoder import views # Importamos las vistas
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     
     path('inicio/', views.inicio, name="Inicio"),
+
     path('jugadores/', views.jugadores, name="Jugadores"),
-    path('equipos/', views.equipos, name="Equipos"),
+    path('jugadorFormulario', views.jugadorFormulario, name="JugadorFormulario"),
+    path('leerJugadores', views.leerJugadores, name="LeerJugadores"),
+    path('eliminarJugador/<numero_para_borrar>/', views.eliminarJugador, name="EliminarJugador"),
+    path('editarJugador/<numero_para_editar>/', views.editarJugador, name="EditarJugador"),
+
     path('estadios/', views.estadios, name="Estadios"),
     path('estadioFormulario/', views.estadioFormulario, name="EstadioFormulario"),
+
     path('ligas/', views.ligas, name="Ligas"),
     path('ligaFormulario/', views.ligaFormulario, name="LigaFormulario"),
-    path('arbitros/', views.arbitros, name="Arbitros"),
-    path('arbitroFormulario/', views.arbitroFormulario, name="ArbitroFormulario"),
-    path('selecciones/', views.selecciones, name="Selecciones"),
-    path('seleccionFormulario/', views.seleccionFormulario, name="SeleccionFormulario"),
 
+    path('quienesSomos/', views.quienesSomos, name="QuienesSomos"),
+
+    path('login', views.login_request, name="Login"),
+    path('register', views.register, name="Register"),
+    path('logout', LogoutView.as_view(template_name='AppCoder/logout.html'), name="Logout"),
+    path('editarPerfil', views.editarPerfil, name="EditarPerfil"),
+
+    path('agregarAvatar', views.agregarAvatar, name="AgregarAvatar"),
+    
+    
 
 
 ]

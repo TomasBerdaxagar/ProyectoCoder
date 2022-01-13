4ace84cac8fc25c81052366a9f46ef8429e03dd0
TomasBerdaxagar <94941276+TomasBerdaxagar@users.noreply.github.com>
2022-01-12 22:02:51 -0300

    Update models.py

--- ProyectoCoder-main/ProyectoCoder/AppCoder/models.py
+++ ProyectoCoder-main/ProyectoCoder/AppCoder/models.py
@@ -1,15 +1,10 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from django.db.models.fields.files import ImageField
+
 # Create your models here.
-class Curso(models.Model):
-    
-    nombre = models.CharField(max_length=40)
-    camada = models.IntegerField()
-    esNoche = models.BooleanField(null=True)
-    
-    def __str__(self) :
-        
-        return f"CURSO {self.nombre} --- CAMADA {self.camada} --- ESNOCHE {self.esNoche}"
+
 class Jugador(models.Model):
     
     apellido= models.CharField(max_length=40)
@@ -19,15 +14,7 @@ class Jugador(models.Model):
     def __str__(self) :
         
         return f"APELLIDO {self.apellido} --- NUMERO {self.numero} --- ESBUENO {self.esBueno}"
-class Equipo(models.Model):
-    
-    nombre=models.CharField(max_length=40)
-    ciudad=models.CharField(max_length=40)
-    nombreHinchada=models.CharField(max_length=40)
-    
-    def __str__(self) :
-        
-        return f"NOMBRE {self.nombre} --- CIUDAD {self.ciudad} --- NOMBREHINCHADA {self.nombreHinchada}"
+
 class Estadio(models.Model):
     
     nombre=models.CharField(max_length=40)
@@ -50,33 +37,21 @@ class Liga(models.Model):
         return f"NOMBRE {self.nombre} --- CANTIDAD DE EQUIPOS {self.cantidadDeEquipos} --- PAIS {self.pais}" 
 
 
-class Arbitro(models.Model):
+class Avatar(models.Model):
+    
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    imagen = models.ImageField(upload_to='avatares', null=True, blank = True)
 
-    nombre=models.CharField(max_length=40)
-    nacionalidad=models.CharField(max_length=40)
-    edad=models.IntegerField()
+
+class QuienesSomos(models.Model):
+    
+    nombre= models.CharField(max_length=40)
+    apellido= models.CharField(max_length=40)
+    
+    
     
     def __str__(self) :
         
-        return f"NOMBRE {self.nombre} --- NACIONALIDAD {self.nacionalidad} --- EDAD {self.edad} --- DIRIGEBIEN {self.dirigeBien}" 
-
-class Seleccion(models.Model):
-    
-    nombre=models.CharField(max_length=40)
-    continente=models.CharField(max_length=40)
-    tecnico=models.CharField(max_length=40)
-    
-    def __str__(self) :
-        
-        return f"NOMBRE {self.nombre} --- CONTINENTE {self.continente} --- TECNICO {self.tecnico}" 
-    
- 
-class Arbitro(models.Model):
-
-    nombre=models.CharField(max_length=40)
-    nacionalidad=models.CharField(max_length=40)
-    edad=models.IntegerField()
-    dirigeBien= models.BooleanField()
-    def __str__(self) :
-        
-        return f"NOMBRE {self.nombre} --- NACIONALIDAD {self.nacionalidad} --- EDAD {self.edad} --- DIRIGEBIEN {self.dirigeBien}" 
+        return f"NOMBRE {self.nombre} --- APELLIDO {self.apellido}"
